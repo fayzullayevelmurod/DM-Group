86de8a4b859265167ef775e14cb968593ce2b150
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-27 10:04:26 +0500

    repair page home desktop in progress

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let swiper1 = new Swiper(".repair_homeSwiper", {
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".repair-button-next",
+      prevEl: ".repair-button-prev",
+    },
+});
