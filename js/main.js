22aa2cd88f2f3b1ddbeda7e3f08fc0bae2f4fac4
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-29 10:55:37 +0500

    accordion media the end

--- js/main.js
+++ js/main.js
@@ -24,3 +24,20 @@ let swiper3 = new Swiper(".examples_Swiper", {
     prevEl: ".examples_Swiper-prev",
   },
 });
+
+const accSingleTriggers = document.querySelectorAll('.js-acc-single-trigger');
+
+accSingleTriggers.forEach(trigger => trigger.addEventListener('click', toggleAccordion));
+
+function toggleAccordion() {
+  const items = document.querySelectorAll('.js-acc-item');
+  const thisItem = this.parentNode;
+
+  items.forEach(item => {
+    if (thisItem == item) {
+      thisItem.classList.toggle('is-open');
+      return;
+    }
+    item.classList.remove('is-open');
+  });
+}
